b9d6c8b4c91be6518f2dc54cb9cbd00f0b35379e
Joe <Walktheworld@me.com>
2021-04-22 14:25:11 -0600

    commit tree 90b8982e62475db897997177dadda2f70fd32314

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
+function findMatching(collection, value) {
+  const newCollection = collection.filter(person => person === value);
+  return newCollection;
+}
+
+function fuzzyMatch(collection, value) {
+  const newCollection = collection.filter((person) => person === value);
+  return newCollection;
+}
+
+function matchName(collection, value) {
+  return collection.filter( data => data.name === value)
+} 
+
+
+
+// function findMatching (collection, value) {
+//     const newCollection = [];
+  
+//     for (const person of collection) {
+//       if (person === value) {
+//         newCollection.push(person);
+//       }else(false)
+//     }
+  
+//     return newCollection;
+// }
+
+
+// function fuzzyMatch(collection, value) {
+//   const nextCollection = [];
+
+//   for (const person of collection) {
+//     if (person === value) {
+//       nextCollection.push(person);
+//     }
+//   }
+
+//   return nextCollection;
+// }
+
 // Code your solution here
+// function findMatching (collection, attribute, value) {
+//     for (const name of collection) {
+//         if (name[attribute] === value){
+//         return [name];
+//         }
+//     }
+// }
